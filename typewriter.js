c49f5e381aac0cf7e04eab05a1be72ac886bf3f5
WishNavy <du.vyshu@gmail.com>
2024-02-16 11:28:24 -0800

    tried setTimeout function

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 1000;
+for (const char of sentence) {
+    setTimeout(() => {
+        process.stdout.write(char);
+    }, delay)
+    delay += 50;
+}
+setTimeout(() => {
+    process.stdout.write('\n');
+}, delay)
